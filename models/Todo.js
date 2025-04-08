7a2e3041a7da554181c8cd1bcea436e02053cb5e
bilalahmet.akpinar <akpinarbilalahmet@gmail.com>
2025-04-08 14:17:06 +0300

    bugs continue..

--- models/Todo.js
+++ models/Todo.js
@@ -1,22 +1,18 @@
-const { DataTypes } = require("sequelize");
+const { DataTypes } = require('sequelize');
 const sequelize = require("./index");
 
-const Todo = sequelize.define(
-  "Todo",
-  {
-    task: {
+const Todo = sequelize.define('Todo', {
+  task: {
       type: DataTypes.STRING,
-      allowNull: false,
-    },
-    userID: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    }
+      allowNull: false
   },
-  {
-    tableName: "Todos",
-    timestamps: true,
+  userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
   }
-);
+}, {
+  tableName: 'Todos',
+  timestamps: true
+});
 
 module.exports = Todo;
